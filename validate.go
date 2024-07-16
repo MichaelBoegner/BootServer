d19c9773b790ed120f00da541a16299f8f772ff6
Charles Michael Boegner <mboegner@charles-boegner.tail16562.ts.net>
2024-07-16 16:23:51 -0400

    wrote further validation to censor pretend curse words. Need to dry up code with helper functions

--- validate.go
+++ validate.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strings"
 )
 
 func (cfg *apiConfig) handlerJSON(w http.ResponseWriter, r *http.Request) {
@@ -23,32 +24,46 @@ func (cfg *apiConfig) handlerJSON(w http.ResponseWriter, r *http.Request) {
 	}
 
 	type returnVals struct {
-		Error string `json:"error"`
-		Valid bool   `json:"valid"`
+		Error        string `json:"error"`
+		Cleaned_body string `json:"cleaned_body"`
 	}
 
 	if len(params.Body) <= 140 {
-		respBody := returnVals{
-			Valid: true,
+		splitBody := strings.Split(params.Body, " ")
+
+		for i, word := range splitBody {
+			word = strings.ToLower(word)
+			if word == "kerfuffle" || word == "sharbert" || word == "fornax" {
+				splitBody[i] = "****"
+			}
 		}
+		joinedBody := strings.Join(splitBody, " ")
+
+		respBody := returnVals{
+			Cleaned_body: joinedBody,
+		}
+
 		data, err := json.Marshal(respBody)
 		if err != nil {
 			log.Printf("Error marshalling JSON: %s", err)
 			w.WriteHeader(500)
 			return
 		}
+
 		w.WriteHeader(http.StatusOK)
 		w.Write(data)
 	} else {
 		respBody := returnVals{
 			Error: "Body is too long",
 		}
+
 		data, err := json.Marshal(respBody)
 		if err != nil {
 			log.Printf("Error marshalling JSON: %s", err)
 			w.WriteHeader(500)
 			return
 		}
+
 		w.WriteHeader(400)
 		w.Write(data)
 	}
