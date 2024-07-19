5d27aefcadf678d8ffe34b9dacaf9fa7a31ee054
Charles Michael Boegner <mboegner@charles-boegner.tail16562.ts.net>
2024-07-19 16:36:55 -0400

    rewrote NewDB to instantiate DBStructure struct and marshal for writing and then LoadDB() unmarshals file to *DBStructure and returns

--- database/database.go
+++ database/database.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"sync"
@@ -23,32 +24,46 @@ func NewDB(path string) (*DB, error) {
 	db := &DB{}
 	db.path = path
 	db.mux = &sync.RWMutex{}
+	db.mux.Lock()
+	defer db.mux.Unlock()
+
 	data, err := os.ReadFile("database/database.json")
 	fmt.Printf("THIS IS ERR == %v, THIS IS DATA == %v", err, data)
+
+	databaseStructure := DBStructure{
+		Chirps: make(map[int]Chirp),
+	}
+
 	if err != nil {
-		err := os.WriteFile(db.path, []byte("My first Chrip."), 0666)
+		marshaledData, err := json.Marshal(databaseStructure)
+		if err != nil {
+			return nil, err
+		}
+		err = os.WriteFile(db.path, []byte(marshaledData), 0666)
 		return db, err
 	} else {
+		marshaledData, err := json.Marshal(databaseStructure)
+		if err != nil {
+			return nil, err
+		}
 		os.Remove("database/database.json")
-		err := os.WriteFile(db.path, []byte("My first Chrip."), 0666)
+		err = os.WriteFile(db.path, []byte(marshaledData), 0666)
 		return db, err
 	}
 }
 
-func (db *DB) LoadDB() (DBStructure, error) {
+func (db *DB) LoadDB() (*DBStructure, error) {
 	db.mux.Lock()
 	defer db.mux.Unlock()
 
 	data, err := os.ReadFile(db.path)
 
-	databaseStructre := DBStructure{
-		Chirps: make(map[int]Chirp),
-	}
-	databaseStructre.Chirps[0] = Chirp{Body: string(data)}
+	databaseStructure := &DBStructure{}
+	err = json.Unmarshal(data, databaseStructure)
 
 	if err != nil {
-		return databaseStructre, err
+		return nil, err
 	}
 
-	return databaseStructre, nil
+	return databaseStructure, nil
 }
